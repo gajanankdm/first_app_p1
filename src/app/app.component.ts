22e4a7ec468bbc2fbe1bf5cb04b889f19cf19689
gajanankdm <148755555+gajanankdm@users.noreply.github.com>
2025-02-28 00:33:15 +0530

    angular intro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ourfirstApp';
+  title = 'I love angular from Ts file !!!';
+  skills : Array<string> =["JS","TS","Angular","Node"]
+subjects : Array<string> =["english","marathi","hindi","biology","chemistry"]
+animals : Array<string> =["Fox", "Lion", "Snake", "Tiger", "monkey", ]
+
 }
